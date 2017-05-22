53268fea10494c65d25586378501eb012d37377c
blackout314 <blackout_lair@yahoo.com>
2017-05-22 08:33:05 -0700

    update gitupdater

--- gitupdater.sh
+++ gitupdater.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+clear
+
+echo "
+                 _       _
+ _   _ _ __   __| | __ _| |_ ___ _ __
+| | | | '_ \ / _' |/ _' | __/ _ | '__|
+| |_| | |_) | (_| | (_| | ||  __| |
+\__,_ | .__/ \__,_|\__,_|\__\___|_|
+      |_|
+"
+
 MYDIR="~/GIT"
 
 DIRS=`ls -l $MYDIR | egrep '^d' | awk '{print $9}'`
@@ -8,5 +19,6 @@ for DIR in $DIRS
 do
   echo "[>] GIT " ${DIR}
   cd ${MYDIR}/${DIR}
-  git pull
+  git pull | sed 's/Already up-to-date./OK/'
+  # git archive HEAD --format=tar |gzip > utils.tgz
 done
